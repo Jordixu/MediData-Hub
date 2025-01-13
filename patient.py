852723d30a80e2f08a8f690a73694edee14c8cc9
Jordi Xu <jordixuwang@gmail.com>
2025-01-13 17:10:42 +0100

    Adds additional attributes to Patient class
    Includes weight, height, assigned doctor, status, medication, and allergies attributes
    Replaces medical history and appointments with previous consults and diagnostics
    
    Adds additional attributes to Patient class
    
    Includes weight, height, assigned doctor, status, medication, and allergies attributes.
    Replaces medical history and appointments with previous consults and diagnostics.

--- patient.py
+++ patient.py
@@ -1,8 +1,13 @@
 from person import Person
 
 class Patient(Person):
-    def __init__(self, id, name, surname)
+    def __init__(self, id, name, surname, weight, height, assigned_doctor):
         super().__init__(id, name, surname)
-        self.medical_history = []
-        self.appointments = []
-        
+        self.previous_consults = []
+        self.previous_diagnostics = []
+        self.weight = weight
+        self.height = height
+        self.assigned_doctor = assigned_doctor
+        self.status = None
+        self.medication = []
+        self.allergies = []
