2cf565f83d35e672a3a5c527a872b979e1b4757f
Jordi Xu <jordixuwang@gmail.com>
2025-01-13 16:49:02 +0100

    Added foundation files
    Created skeletons of hospital and person class

---
+++ hospital.py
@@ -0,0 +1,28 @@
+class Hospital():
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        
+    def add_patient(patient):
+        pass
+    
+    def remove_patient(patient):
+        pass
+    
+    def add_doctor(doctor):
+        pass
+
+    def remove_doctor(doctor):
+        pass
+    
+    def schedule_appointment(patient, doctor, date):
+        pass
+    
+    def cancel_appointment(patient, doctor, date):
+        pass
+    
+    def add_space(space):
+        pass
+    
+    def remove_space(space):
+        pass
---
+++ person.py
@@ -0,0 +1,15 @@
+class Person():
+    def __init__(self, id, name, surname, age, sex):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.age = age
+        
+    def get_info(self, attribute):
+        return self.attribute
+    
+    def set_info(self, attribute):
+        self.attribute = attribute
+        
+    def display_info(self):
+        pass
