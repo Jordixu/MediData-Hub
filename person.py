af53ea1f6159a71ef2eef7ff88c087d792326a53
Jordi Xu <jordixuwang@gmail.com>
2025-01-19 00:23:37 +0100

    Refactor Person class to replace 'sex' with 'gender' parameter; enhance methods for better error handling and notification management

--- person.py
+++ person.py
@@ -1,10 +1,10 @@
 class Person():
-    def __init__(self, id, name, surname, age, sex):
+    def __init__(self, id, name, surname, age, gender):
         self.id = id
         self.name = name
         self.surname = surname
         self.age = age
-        self.sex = sex
+        self.gender = gender
         self.notifications = []
         
     def get_info(self, attribute):
@@ -28,3 +28,37 @@ class Person():
         
     def display_last_notification(self):
         return self.notifications[-1]
+    
+class Person:
+    def __init__(self, id, name, surname, age, gender):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.sex = gender
+        self.notifications = []
+
+    def get_info(self, attribute):
+        return getattr(self, attribute, 'Attribute not found')
+
+    def set_info(self, attribute, value):
+        try:
+            if type(getattr(self, attribute)) in [str, bool, float, int]:
+                setattr(self, attribute, value)
+            elif type(getattr(self, attribute)) in [list, set, dict]:
+                getattr(self, attribute).append(value)
+        except AttributeError:
+            print('Attribute not found')
+        except TypeError:
+            print('Invalid value')
+        except Exception as e:
+            print(f'An error occurred: {e}')
+
+    def add_notification(self, notification):
+        self.notifications.append(notification)
+
+    def display_last_notification(self):
+        if self.notifications:
+            return self.notifications[-1]
+        else:
+            return 'No notifications'
