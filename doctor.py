ba8bce6c3a9dc0967413f5cb5ec526cd0c022748
Jordi Xu <jordixuwang@gmail.com>
2025-01-21 19:17:33 +0100

    Refactor Doctor class to use private attributes and improve appointment management methods

--- doctor.py
+++ doctor.py
@@ -3,56 +3,68 @@ from notification import Notification
 
 class Doctor(Person):
     def __init__(self, personal_id, password, name, surname, age, gender, speciality, department, socialsecurity, salary, hospital_id = None):
-        super().__init__(personal_id, hospital_id, password, name, surname, age, gender, notifications = [])
-        self.speciality = speciality
-        self.department = department
-        self.availability = {}
-        self.socialsecurity = socialsecurity
-        self.salary = salary
-        self.assigned_patients = []
-        
+        super().__init__(personal_id, hospital_id, password, name, surname, age, gender)
+        self._speciality = speciality
+        self._department = department
+        self._availability = {}
+        self._socialsecurity = socialsecurity
+        self._salary = salary
+        self._assigned_patients = []
+        self._notifications = []
+        self._appointments = []
+    
     def __str__(self):
         return f'Dr. {self.name} {self.surname}'
-        
-    def add_appointment(self, appointment):
-        self.schedule.append(appointment)
     
-    def remove_appointment(self, date, last_appointment):
+    def add_appointment(self, appointment):
+        self._appointments.append(appointment)
+        
+    def remove_last_appointment(self, appointment, last_appointment):
         if last_appointment:
-            self.schedule.pop()
+            self._appointments.pop()
         elif not last_appointment:
-            for appointment in self.schedule:
-                if appointment.date == date:
-                    self.schedule.remove(appointment)
+            for app in self._appointments:
+                if app == appointment:
+                    self._appointments.remove(app)
                     
     def change_speciality(self, speciality):
-        self.speciality = speciality
+        self._speciality = speciality
     
     def change_department(self, department):
-        self.department = department
+        self._department = department
     
     def change_socialsecurity(self, socialsecurity):
-        self.socialsecurity = socialsecurity
+        self._socialsecurity = socialsecurity
     
     def change_salary(self, salary):
-        self.salary = salary
+        self._salary = salary
         
     def add_patient(self, patient):
-        self.assigned_patients.append(patient)
+        self._assigned_patients.append(patient)
         
     def remove_patient(self, patient, last_patient):
         if last_patient:
-            self.assigned_patients.pop()
+            self._assigned_patients.pop()
         elif not last_patient:
-            for pat in self.assigned_patients:
+            for pat in self._assigned_patients:
                 if pat == patient:
-                    self.assigned_patients.remove(pat)
+                    self._assigned_patients.remove(pat)
                     
     def create_schedule(self, date, timeframes):
-        if date not in self.availabilities:
-            self.availabilities[date] = {}
+        if date not in self._availability:
+            self._availability[date] = {}
         for timeframe in timeframes:
-            self.availabilities[date][timeframe] = True
+            self._availability[date][timeframe] = True
     
     def display_schedule(self):
-        return self.availabilities
+        return self._availability
+    
+    def check_availability(self, date, timeframe):
+        if date in self._availability:
+            if timeframe in self._availability[date]:
+                return self._availability[date][timeframe]
+        return False
+    
+    def change_availability(self, date, timeframe):
+        if date in self._availability and timeframe in self._availability[date]:
+            self._availability[date][timeframe] = not self._availability[date][timeframe]
