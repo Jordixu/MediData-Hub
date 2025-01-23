029e6eadb94b0544622a079b299d9b73e955d637
Jordi Xu <jordixuwang@gmail.com>
2025-01-23 16:19:53 +0100

    Refactor role selection screen layout for improved readability and organization

--- ui/roleSelectionScreen.py
+++ ui/roleSelectionScreen.py
@@ -6,35 +6,37 @@ class RoleSelectionScreen(ctk.CTkFrame):
         super().__init__(parent)
         self.controller = controller
         self.title = "Role Selection"
-
-        # Configure the grid
-        for i in range(5):
-            self.grid_rowconfigure(i, weight=1)
-        for j in range(5):
-            self.grid_columnconfigure(j, weight=1)
-
+        
+        # Empty space
+        empty_frame = ctk.CTkFrame(self, fg_color="transparent", height=150)
+        empty_frame.pack()
+        
         # Title
-        label = ctk.CTkLabel(self, text="Select Role", font=("Helvetica", 24, "bold"))
-        label.grid(row=1, column=2, pady=10)
+        title_frame = ctk.CTkFrame(self, fg_color="transparent")
+        title_frame.pack(pady=20)
+        label = ctk.CTkLabel(title_frame, text="Select Role", font=("Helvetica", 24, "bold"))
+        label.grid(row=0, column=2, pady=10)
 
         # Buttons
+        buttons_frame = ctk.CTkFrame(self, fg_color="transparent")
+        buttons_frame.pack(pady=20)
         ctk.CTkButton(
-            self, text="Patient",
+            buttons_frame, text="Patient",
             command=lambda: self.select_role("patient"),
-            width=200
-        ).grid(row=2, column=1, pady=20, padx=20)
+            width=200, height=50
+        ).grid(row=0, column=1, pady=20, padx=20)
 
         ctk.CTkButton(
-            self, text="Doctor",
+            buttons_frame, text="Doctor",
             command=lambda: self.select_role("doctor"),
-            width=200
-        ).grid(row=2, column=2, pady=20, padx=20)
+            width=200, height=50
+        ).grid(row=0, column=2, pady=20, padx=20, sticky="ns")
 
         ctk.CTkButton(
-            self, text="Administrator",
+            buttons_frame, text="Administrator",
             command=self.not_implemented,
-            width=200
-        ).grid(row=2, column=3, pady=20, padx=20)
+            width=200, height=50
+        ).grid(row=0, column=3, pady=20, padx=20, sticky="ns")
 
     def select_role(self, role):
         self.controller.selected_role = role
