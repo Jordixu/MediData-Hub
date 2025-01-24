669e127f8dfb46742dab7b98474f095938acaf7a
Jordi Xu <jordixuwang@gmail.com>
2025-01-24 04:22:49 +0100

    Refactor Hospital class to improve encapsulation and enhance attribute management

--- hospital.py
+++ hospital.py
@@ -15,8 +15,8 @@ class Hospital:
             location (str): The location of the hospital.
         """
         
-        self._name = name
-        self._location = location 
+        self.__name = name
+        self.__location = location 
         self.patients = []
         self.doctors = []
         self.appointments = []
@@ -27,39 +27,64 @@ class Hospital:
         try:
             patients = Data.load_from_csv('./database/patients.csv')
             for patient in patients:
-                self.patients.append(Patient(**patient))
+                patient['password'] = patient.pop('_password', None)
+                patient['notifications'] = patient.pop('_Patient__notifications', None)
+                patient['appointments'] = patient.pop('_Patient__appointments', None)
+                patient['medications'] = patient.pop('_Patient__medications', None)
+                patient['allergies'] = patient.pop('_Patient__allergies', None)
+                patient['diagnoses'] = patient.pop('_Patient__diagnoses', None)
+                new_patient = Patient(**patient)
+                self.patients.append(new_patient)
+                print(new_patient)
         except FileNotFoundError:
+            print("No patients found in the database")
             pass
-            # raise FileNotFoundError('No patients found in the database')
-        
+        except TypeError:
+            print(f"{patient} is not a patient")
+            pass
+
         try:
             doctors = Data.load_from_csv('./database/doctors.csv')
             for doctor in doctors:
+                doctor['_Person__password'] = doctor.pop('_password', None)
+                doctor['_Doctor__availability'] = doctor.pop('_availability', None)
+                doctor['_Doctor__assigned_patients'] = doctor.pop('_assigned_patients', None)
+                doctor['_Doctor__notifications'] = doctor.pop('_notifications', None)
+                doctor['_Doctor__appointments'] = doctor.pop('_appointments', None)
                 self.doctors.append(Doctor(**doctor))
         except FileNotFoundError:
+            print("No doctors found in the database")
             pass
-            # raise FileNotFoundError('No doctors found in the database')
-        
+        except TypeError:
+            print("Incorrect type")
+            pass
+
         try:
             for appointment in Data.load_from_csv('./database/appointments.csv'):
                 self.appointments.append(Appointment(**appointment))
         except FileNotFoundError:
+            print("No appointments found in the database")
             pass
-            # raise FileNotFoundError('No appointments found in the database')
-        
+        except TypeError:
+            print("Incorrect type")
+            pass
+
         try:
             for room in Data.load_from_csv('./database/rooms.csv'):
                 self.rooms.append(Room(**room))
         except FileNotFoundError:
+            print("No rooms found in the database")
             pass
-            # raise FileNotFoundError('No rooms found in the database')
-        
+        except TypeError:
+            print("Incorrect type")
+            pass
+
         # try:
         #     for drug in Data.load_from_csv('./database/drugs.csv'):
         #         self.drugs.append(Drug(**drug))
         # except FileNotFoundError:
         #     raise FileNotFoundError('No drugs found in the database')
-        
+
     #@staticmethod
     def validate_value(self, value: any,value_type: object, lower: int|float = None, upper: int|float = None, custom_message_incorrect_type: str = None, custom_message_lower: str = None, custom_message_upper: str = None) -> object: # quizás integremos esto fuera de la clase Hospital o lo definimos como staticmethod para usarlo fuera
         """
@@ -100,7 +125,7 @@ class Hospital:
             personal_id (int): Unique identifier for the patient. Must be a positive integer.
             name (str): The patient's first name.
             surname (str): The patient's last name.
-            age (int): Age of the patient. Must be a positive integer within a plausible range.
+            birthday (int): Birthday of the patient. Must be a date within a plausible range.
             weight (float): Weight of the patient in kilograms. Must be a positive float within a plausible range.
             height (float): Height of the patient in centimeters. Must be a positive float within a plausible range.
             assigned_doctor (Doctor): The doctor assigned to the patient.
@@ -114,9 +139,11 @@ class Hospital:
         """
         
         kwargs['personal_id'] = self.validate_value(kwargs['personal_id'], int, 0, custom_message_incorrect_type="The ID must be a number, without decimals.", custom_message_lower="The ID must be a positive number.")
-        kwargs['age'] = self.validate_value(kwargs['age'], int, 0, 150, custom_message_incorrect_type="The age is a number...", custom_message_lower="The age must be a positive number.", custom_message_upper="I don't think you are that old.")
         kwargs['weight'] = self.validate_value(kwargs['weight'], float, 0, 1000, custom_message_incorrect_type="The weight must be a number...", custom_message_lower="The weight must be a positive number.", custom_message_upper="Did you know that the heaviest person ever recorded was 635 kg?, either you are lying or you are a record breaker.")
         kwargs['height'] = self.validate_value(kwargs['height'], float, 0, 300, custom_message_incorrect_type="The height must be a number...", custom_message_lower="The height must be a positive number.", custom_message_upper="Hello, Mr. giant, how can I help you?")
+        
+        if not (dt.date(1900, 1, 1) <= kwargs['birthday'] <= dt.date.today()):
+            raise ValueError("The birthday must be between 1900-01-01 and today.")
         
         if any(p.personal_id == kwargs['personal_id'] for p in self.patients):
             raise ValueError("The ID is already in our system, perhaps there is a typo?")
@@ -157,7 +184,7 @@ class Hospital:
             hospital_id (int): Unique hospital identifier for the doctor. Generated automatically.
             name (str): The doctor's first name.
             surname (str): The doctor's last name.
-            age (int): Age of the doctor. Must be a positive integer within a plausible
+            birthday (int): Birthday of the doctor. Must be a date within a plausible range.
             gender (str): The doctor's gender.
             speciality (str): The doctor's speciality.
             department (str): The department the doctor belongs to.
@@ -172,7 +199,7 @@ class Hospital:
         """
         kwargs['personal_id'] = self.validate_value(kwargs['personal_id'], int, 0)
         kwargs['hospital_id'] = self.validate_value(kwargs['hospital_id'], int, 0)
-        kwargs['age'] = self.validate_value(kwargs['age'], int, 0, 150)
+        kwargs['birthday'] = self.validate_value(kwargs['birthday'], dt.date, 0, 150)
         kwargs['socialsecurity'] = self.validate_value(kwargs['socialsecurity'], int)
         kwargs['salary'] = self.validate_value(kwargs['salary'], float, 0)
         
@@ -209,8 +236,12 @@ class Hospital:
         return doctor.check_availability(date, timeframe)
 
     def schedule_appointment(self, patient: Patient, doctor: Doctor, date: str, timeframe: tuple) -> str:
+        
+        # Check if the doctor exists
         if doctor not in self.doctors:
-            return 'Doctor not found, please introduce the correct ID'
+            return 'Doctor not found'
+        
+        # Check if the doctor is available
         if not doctor.check_availability(doctor, date, timeframe):
             return f'Dr. {doctor.surname} is not available at that time, please choose another date or timeframe'
 
@@ -221,21 +252,23 @@ class Hospital:
                 appointment_id = self.appointments[-1].id + 1 if self.appointments else 1
                 appointment = Appointment(appointment_id, date, timeframe, doctor, patient, room, 'scheduled')
                 self.appointments.append(appointment)
-                doctor.add_appointment(appointment)
-                patient.add_appointment(appointment)
+                doctor.add_appointment({appointment.get('appointment_id'): appointment.__status})
+                patient.add_appointment(appointment.get('appointment_id'))
         return 'No available rooms, please choose another date or timeframe'
 
     def cancel_appointment(self, patient: Patient, doctor: Doctor, date: str, timeframe: tuple) -> str:
         for appointment in self.appointments:
             if appointment.patient == patient and appointment.doctor == doctor and appointment.date == date and appointment.timeframe == timeframe:
-                self.appointments.remove(appointment)
                 doctor.availabilities[date][timeframe] = True
                 self.spaces[appointment.room].availability[date][timeframe] = True
+                appointment.change_status('cancelled')
                 doctor.appointment(appointment).change_status('cancelled')
                 patient.appointment(appointment).change_status('cancelled')
 
-                self._send_notification(doctor, f'Appointment cancelled for {date} at {timeframe[0]} for patient {patient.name} {patient.surname}')
-                self._send_notification(patient, f'Appointment cancelled for {date} at {timeframe[0]} by Dr. {doctor.name} {doctor.surname}')
+                self.send_notification(doctor, f'Appointment cancelled for {date} at {timeframe[0]} for patient {patient.name} {patient.surname}')
+                self.send_notification(patient, f'Appointment cancelled for {date} at {timeframe[0]} by Dr. {doctor.name} {doctor.surname}')
                 return f'Appointment cancelled for {date}'
         return 'Appointment not found'
     
+    def send_notification(self, person, message):
+        person.add_notification(Notification(message))
