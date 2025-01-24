4168bc7cafa06fada6913cc6907cc701a1ade781
Jordi Xu <jordixuwang@gmail.com>
2025-01-24 04:22:39 +0100

    Refactor Doctor class to improve encapsulation and enhance attribute management

--- doctor.py
+++ doctor.py
@@ -1,70 +1,80 @@
 from person import Person
 from notification import Notification
+from datetime import date
 
 class Doctor(Person):
-    def __init__(self, personal_id, password, name, surname, age, gender, speciality, department, socialsecurity, salary, hospital_id = None):
-        super().__init__(personal_id, hospital_id, password, name, surname, age, gender)
-        self._speciality = speciality
-        self._department = department
-        self._availability = {}
-        self._socialsecurity = socialsecurity
-        self._salary = salary
-        self._assigned_patients = []
-        self._notifications = []
-        self._appointments = []
+    def __init__(self, 
+            personal_id: int,
+            hospital_id: int,
+            password: str, 
+            name: str, 
+            surname: str, 
+            birthday: date, 
+            gender: str, 
+            speciality: str, 
+            department: str, 
+            socialsecurity: str, 
+            salary: float,  
+            availability: dict = None, 
+            assigned_patients: list = None, 
+            notifications: list = None, 
+            appointments: list = None
+            ) -> None:
+        
+        super().__init__(personal_id, hospital_id, password, name, surname, birthday, gender, notifications)
+        self.speciality = speciality
+        self.department = department
+        self.__availability = availability
+        self.socialsecurity = socialsecurity
+        self.salary = salary
+        self.__assigned_patients = assigned_patients
+        self.__notifications = notifications
+        self.__appointments = appointments
     
     def __str__(self):
         return f'Dr. {self.name} {self.surname}'
     
     def add_appointment(self, appointment):
-        self._appointments.append(appointment)
-        
-    # def remove_last_appointment(self, appointment, last_appointment):
-    #     if last_appointment:
-    #         self._appointments.pop()
-    #     elif not last_appointment:
-    #         for app in self._appointments:
-    #             if app == appointment:
-    #                 self._appointments.remove(app)
+        self.__appointments.append(appointment)
                     
     def change_speciality(self, speciality):
-        self._speciality = speciality
+        self.speciality = speciality
     
     def change_department(self, department):
-        self._department = department
+        self.department = department
     
     def change_socialsecurity(self, socialsecurity):
-        self._socialsecurity = socialsecurity
+        self.socialsecurity = socialsecurity
     
     def change_salary(self, salary):
-        self._salary = salary
+        self.salary = salary
         
     def add_patient(self, patient):
-        self._assigned_patients.append(patient)
+        self.__assigned_patients.append(patient)
         
     def remove_patient(self, patient, last_patient):
         if last_patient:
-            self._assigned_patients.pop()
+            self.__assigned_patients.pop()
         elif not last_patient:
-            for pat in self._assigned_patients:
+            for pat in self.__assigned_patients:
                 if pat == patient:
-                    self._assigned_patients.remove(pat)
+                    self.__assigned_patients.remove(pat)
                     
     def create_schedule(self, date, timeframes):
-        if date not in self._availability:
-            self._availability[date] = {}
+        if date not in self.__availability:
+            self.__availability[date] = {}
         for timeframe in timeframes:
-            self._availability[date][timeframe] = True
+            self.__availability[date][timeframe] = True
     
     def display_schedule(self):
-        return self._availability
+        return self.__availability
     
     def check_availability(self, date, timeframe):
-        if date in self._availability:
-            if timeframe in self._availability[date]:
-                return self._availability[date][timeframe]
+        if date in self.__availability:
+            if timeframe in self.__availability[date]:
+                return self.__availability[date][timeframe]
         return False
     
     def change_availability(self, date, timeframe):
-        if date in self._availability and timeframe in self._availability[date]:
-            self._availability[date][timeframe] = not self._availability[date][timeframe]
+        if date in self.__availability and timeframe in self.__availability[date]:
+            self.__availability[date][timeframe] = not self.__availability[date][timeframe]
