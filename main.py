9d40a0d5d06dd4f9149df9dafce136c06f8cf264
Jordi Xu <jordixuwang@gmail.com>
2025-01-25 20:40:16 +0100

    Update main.py to integrate DataGenerator for patient, doctor, room, and appointment creation; adjust hospital instantiation for admin access.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from hospital import Hospital
 from ui.medidataHubUI import MedidataHubUI
-from random_datas import generate_fake_data
+from random_datas import DataGenerator
 from datetime import date
 
-
 def main():
-    hospital = Hospital("Medidata Hospital", "Barcelona")
+    # If True generates new data. ALL OLD DATA WILL BE LOST
+    generate_new_data = True
+    
+    if generate_new_data:
+        DataGenerator.generate_patients()
+        DataGenerator.generate_doctors()
+        DataGenerator.generate_rooms()
+        DataGenerator.generate_appointments()
+    
+    hospital = Hospital("Admin", "Admin")
     hospital.load_data()
     ui = MedidataHubUI(hospital)
     ui.mainloop()
