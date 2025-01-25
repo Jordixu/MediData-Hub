22594f3e4b5b1f1265c36006ff39758360bd47ca
Jordi Xu <jordixuwang@gmail.com>
2025-01-25 20:40:40 +0100

    Add Datetime class with methods for creating timeframes, weeks, and schedules

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
 
 class Data:
     @staticmethod
@@ -43,3 +44,33 @@ class Data:
             )
         # save_to_csv(drugs_dict, './database/drugs.csv')
         
+class Datetime:
+    @staticmethod
+    def create_timeframe():
+        timeframes = []
+        start_time = dt.time(9, 0)
+        end_time = dt.time(21, 0)
+        current_time = start_time
+
+        while current_time < end_time:
+            next_time = (dt.datetime.combine(dt.date.today(), current_time) + dt.timedelta(hours=1)).time()
+            timeframes.append((current_time.strftime("%H:%M"), next_time.strftime("%H:%M")))
+            current_time = next_time
+        return timeframes
+        
+    @staticmethod
+    def create_week():
+        week = []
+        today = dt.date.today()
+        for i in range(7):
+            week.append(today + dt.timedelta(days=i))
+        return week
+    
+    @staticmethod
+    def create_schedule():
+        schedule = {}
+        for day in Datetime.create_week():
+            schedule[day] = {}
+            for timeframe in Datetime.create_timeframe():
+                schedule[day][timeframe] = True
+        return schedule
