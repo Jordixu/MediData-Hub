1aa9d5badb0fd084a682c3bf0353df92904e6b92
Jordi Xu <jordixuwang@gmail.com>
2025-01-26 03:59:55 +0100

    Refactor Appointment class to improve attribute handling and documentation

--- appointment.py
+++ appointment.py
@@ -3,31 +3,57 @@ from doctor import Doctor
 from patient import Patient
 from room import Room
 class Appointment():
-    def __init__(self, appointment_id: int, date: date, timeframe: tuple, doctor: Doctor, patient: Patient, room: Room, status:str, diagnosis:str = None, medication = None, notes:str = None) -> None:
+    """
+    Represents an appointment in a hospital.
+    
+    Attributes:
+        appointment_id (int): The ID of the appointment.
+        date (date): The date of the appointment.
+        timeframe (tuple): The time slot for the appointment (start_time, end_time).
+        doctor_hid (int): The ID of the doctor assigned to the appointment.
+        patient_hid (int): The ID of the patient scheduled for the appointment.
+        room_number (int): The number of the room assigned for the appointment.
+        status (str): The current status of the appointment.
+        diagnosis (str): The diagnosis of the patient.
+        medication (str): The medication prescribed to the patient.
+        
+    Methods:
+        get(attribute): Returns the value of the attribute.
+        change_status(status): Changes the status of the appointment.
+        autocomplete(): Automatically changes the status of the appointment to completed if the date and time have passed.
+        change_datetime(date, timeframe): Changes the date and time of the appointment.
+        change_doctor(doctor_hid): Changes the doctor assigned to the appointment.
+        change_patient(patient_hid): Changes the patient scheduled for the appointment.
+        change_room(room_hid): Changes the room assigned for the appointment.
+        get_all_attributes(): Returns all the attributes of the appointment.
+    """
+    def __init__(self, appointment_id: int, date: date, timeframe: tuple, doctor_hid: int, patient_hid: int, room_number: int, status:str, diagnosis:str = None, medication = None) -> None:
         """
         Initialize an Appointment instance.
         Args:
+            appointment_id (int): The ID of the appointment.
             date (date): The date of the appointment.
             timeframe (tuple): The time slot for the appointment (start_time, end_time).
-            doctor (Doctor): The doctor assigned to the appointment.
-            patient (Patient): The patient scheduled for the appointment.
-            room (Room): The room assigned for the appointment.
+            doctor_hid (int): The ID of the doctor assigned to the appointment.
+            patient_hid (int): The ID of the patient scheduled for the appointment.
+            room_number (int): The number of the room assigned for the appointment.
             status (str): The current status of the appointment.
+            diagnosis (str, optional): The diagnosis of the patient. Defaults to None.
+            medication (str, optional): The medication prescribed to the patient. Defaults to None.
         """
 
         self.appointment_id = appointment_id
         self.__date = date
         self.__timeframe = timeframe
-        self.__doctor = doctor
-        self.__patient = patient
-        self.__room = room
+        self.__doctor_hid = doctor_hid
+        self.__patient_hid = patient_hid
+        self.__room_number = room_number
         self.__status = status
         self.__diagnosis = diagnosis
         self.__medication = medication
-        self.__notes = notes
 
     def __str__(self) -> str:
-        return f'{self.__date} at {self.__timeframe} with Dr. {self.__doctor} and {self.__patient} in room {self.__room} is {self.__status}'
+        return f'{self.__date} at {self.__timeframe} with Dr. {self.__doctor_hid} and {self.__patient_hid} in room {self.__room_number} is {self.__status}'
     
     def get(self, attribute: str) -> str:
         try:
@@ -46,8 +72,10 @@ class Appointment():
             if self.__timeframe[1] < datetime.now().time():
                 self.__status = 'completed'
     
-    def change_date(self, date: date) -> str:
+    def change_datetime(self, date: date, timeframe: tuple) -> str: # Check if everything is available
         self.__date = date
+        self.__timeframe = timeframe
+        return f'The appointment is now scheduled on {self.__date} at {self.__timeframe}'
     
     # def change_time(self, timeframe: tuple) -> str:
     #     # Check if the doctor is available
@@ -60,17 +88,27 @@ class Appointment():
     #     self.timeframe = timeframe
     #     return f'The appointment is now scheduled at {self.timeframe}'
     
-    def change_doctor(self, doctor: Doctor) -> str:
-        if doctor == self.__doctor:
-            return f'The appointment is already with Dr. {self.__doctor}'
-        self.__doctor = doctor
-        return f'The appointment is now with Dr. {self.__doctor}'
+    def change_doctor(self, doctor_hid: Doctor) -> str:
+        if doctor_hid == self.__doctor_hid:
+            return f'The appointment is already with Dr. {self.__doctor_hid}'
+        self.__doctor_hid = doctor_hid
+        return f'The appointment is now with Dr. {self.__doctor_hid}'
     
-    def change_patient(self, patient: Patient) -> str:
-        self.__patient = patient
+    def change_patient(self, patient_hid: Patient) -> str:
+        self.__patient_hid = patient_hid
     
-    def change_room(self, room: Room) -> str:
-        self.__room = room
+    def change_room(self, room_number: Room) -> str:
+        self.__room_number = room_number
         
     def get_all_attributes(self) -> dict:
-        return self.__dict__
+        attributes = {}
+        attributes['appointment_id'] = self.appointment_id
+        attributes['date'] = self.__date
+        attributes['timeframe'] = self.__timeframe
+        attributes['doctor_hid'] = self.__doctor_hid
+        attributes['patient_hid'] = self.__patient_hid
+        attributes['room_number'] = self.__room_number
+        attributes['status'] = self.__status
+        attributes['diagnosis'] = self.__diagnosis
+        attributes['medication'] = self.__medication
+        return attributes
