a8705a575a1f5fd947acd8b81dfd41c29da35dd1
Jordi Xu <jordixuwang@gmail.com>
2025-01-26 04:00:31 +0100

    Enhance Person class with detailed docstring, improve private attribute handling, and refine error management in methods

--- person.py
+++ person.py
@@ -1,4 +1,26 @@
 class Person():
+    """
+    The base class for all people in the hospital.
+    
+    Attributes:
+        personal_id (int): The personal ID of the person.
+        hospital_id (int): The hospital ID of the person.
+        password (str): The password for the person's account.
+        name (str): The first name of the person.
+        surname (str): The surname of the person.
+        birthday (date): The birth date of the person
+    
+    Methods:
+        get(attribute): Returns the value of the attribute.
+        get_private_info(attribute): Returns the value of a private attribute.
+        set_private_info(attribute, value): Changes the value of a private attribute.
+        __name__(): Returns the full name of the person.
+        check_password(password): Checks if the password is correct.
+        set_info(attribute, value, type): Changes the value of an attribute.
+        add_notification(notification_id): Adds a notification to the person.
+        display_last_notification(): Displays the last notification the person received.
+        add_appointment(appointment_id): Adds an appointment to the person.
+    """
     def __init__(self, personal_id, hospital_id, password, name, surname, birthday, gender, appointments = None):
         self.personal_id = personal_id
         self.hospital_id = hospital_id
@@ -8,22 +30,23 @@ class Person():
         self.birthday = birthday
         self.gender = gender
         self.appointments = appointments if appointments is not None else {}
+        self.__notifications = []
         
     def get(self, attribute):
         try:
             return getattr(self, attribute)
-        except AttributeError:
-            print('Attribute not found')
-        except TypeError:
-            print('Invalid value')
-        except:
-            print('An error occurred')
+        except AttributeError as exc:
+            raise AttributeError('Attribute not found') from exc
+        except TypeError as exc:
+            raise TypeError('Invalid value') from exc
+        except Exception as exc:
+            raise Exception('An error occurred in getting the attribute') from exc
             
-    def get_password(self):
-        return self.password
+    def get_private_info(self, attribute):
+        return getattr(self, "__" + attribute)
     
-    def set_password(self, password):
-        self.password = password
+    def set_private_info(self, attribute, value):
+        setattr(self, "__" + attribute, value)
         
     def __name__(self):
         return self.name + ' ' + self.surname
@@ -41,21 +64,18 @@ class Person():
                 setattr(self, attribute, float(value))
             elif type == 'bool':
                 setattr(self, attribute, bool(value))
-        except AttributeError:
-            print('Attribute not found')
-        except TypeError:
-            print('Invalid value')
+        except AttributeError as exc:
+            raise AttributeError('Attribute not found') from exc
+        except TypeError as exc:
+            raise TypeError('Invalid value') from exc
         except:
-            print('An error occurred')  
+            raise Exception('An error occurred in setting the attribute')
 
-    def add_notification(self, notification):
-        self.__notifications.append(notification)
+    def add_notification(self, notification_id):
+        self.__notifications.append(notification_id)
 
     def display_last_notification(self):
         return self.__notifications[-1]
-
-    def get_all_attributes(self):
-        return self.__dict__
     
     def add_appointment(self, appointment_id):
         self.appointments.append(appointment_id)
